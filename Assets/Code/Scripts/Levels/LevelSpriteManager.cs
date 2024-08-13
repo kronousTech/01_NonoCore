41c182dd4271cd44c192727d05346c951d4bd1dd
Kronos <joaosantosprofi@protonmail.com>
2024-08-13 08:55:21 +0100

    Frame sprite added.

--- Assets/Code/Scripts/Levels/LevelSpriteManager.cs
+++ Assets/Code/Scripts/Levels/LevelSpriteManager.cs
@@ -27,6 +27,89 @@ public class LevelSpriteManager : MonoBehaviour
     {
         return Sprites[2];
     }
+    public static Sprite GetCounterFrame(LevelSquareCounterType type, int index, int maxSize)
+    {
+        switch (type)
+        {
+            case LevelSquareCounterType.Column:
+                if(index == 0)
+                {
+                    return Sprites[7];
+                }
+                else if(index == maxSize-1)
+                {
+                    return Sprites[9];
+                }
+                else
+                {
+                    return Sprites[8];
+                }
+            case LevelSquareCounterType.Row:
+                if (index == 0)
+                {
+                    return Sprites[10];
+                }
+                else if (index == maxSize-1)
+                {
+                    return Sprites[20];
+                }
+                else
+                {
+                    return Sprites[15];
+                }
+            default:
+                Debug.LogError("LevelSpriteManager.cs: Invalid Square counter type: " + type);
+                return null;
+        }
+    }
+    public static Sprite GetSquareFrame(int x, int y, int maxX, int maxY)
+    {
+        if(y == 0)
+        {
+            if(x == 0)
+            {
+                return Sprites[11];
+            }
+            else if(x == maxX - 1)
+            {
+                return Sprites[13];
+            }
+            else
+            {
+                return Sprites[12];
+            }
+        }
+        else if(y == maxY - 1)
+        {
+            if (x == 0)
+            {
+                return Sprites[21];
+            }
+            else if (x == maxX - 1)
+            {
+                return Sprites[23];
+            }
+            else
+            {
+                return Sprites[22];
+            }
+        }
+        else
+        {
+            if (x == 0)
+            {
+                return Sprites[16];
+            }
+            else if (x == maxX - 1)
+            {
+                return Sprites[18];
+            }
+            else
+            {
+                return Sprites[17];
+            }
+        }
+    }
 
     public static Sprite GetSquareForeground(LevelGridSquareType type)
     {
--- Assets/Code/Scripts/Levels/LevelSquare.cs
+++ Assets/Code/Scripts/Levels/LevelSquare.cs
@@ -9,16 +9,20 @@ namespace KronosTech.Levels
         [Header("References")]
         [SerializeField] private Image _backgroundDisplay;
         [SerializeField] private Image _foregroundDisplay;
+        [SerializeField] private Image _frameDisplay;
         [SerializeField] private TextMeshProUGUI _display;
 
-        public void Initialize(int x, int y)
+        public void Initialize(int x, int y, int maxX, int maxY)
         {
             var type = LevelBuildSettings.GetSquareType(x, y);
             _display.text = type.ToString();
 
+            transform.name = "Square: " + x + "-" + y;
+
             _backgroundDisplay.sprite = LevelSpriteManager.GetSquareBackground();
             _foregroundDisplay.sprite = LevelSpriteManager.GetSquareForeground(type);
             _foregroundDisplay.color = _foregroundDisplay.sprite == null ? Color.clear : Color.white;
+            _frameDisplay.sprite = LevelSpriteManager.GetSquareFrame(x, y, maxX, maxY);
         }
     }
 }
